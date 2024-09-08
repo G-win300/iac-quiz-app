40a777ef80f8d57a604a654f945f5cb33971fc62
testing <testing@gmail.com>
2024-09-08 20:00:52 +0100

    updated pipeline

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -43,8 +43,8 @@ variable "iam-role" {
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  type        = string
-  # default     = "ami-0c7217cdde317cfec" // Replace with the latest AMI ID for your region
+  type        = string 
+  #default     ="ami-0c7217cdde317cfec" // Replace with the latest AMI ID for your region
   default     = "ami-0a0e5d9c7aac336f1" // Replace with the latest AMI ID for your region
 }
 
